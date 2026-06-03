124af51119acc4820d1a5cd12a588b8a32e1fb9e
Abhirag B <bodiabhirag@gmail.com>
2026-06-03 16:21:37 -0500

    added depth to the register and login routes in both the main file and the html files. started saving inputs from the user from those routes as real variables and queried them against the User database made with SQLAlchemy to make early attempts to authenticate the user's credentials, and added debugging prints

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -28,12 +28,41 @@ class User(db.Model):
 def home():
     return render_template("home.html")
 
-@app.route("/register")
+@app.route("/register", methods = ["GET", "POST"])
 def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        print(f"Creating user: {username}")
+
+        new_user = User(
+            username=username,
+            password=password   
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        print("User saved!")
+
+        return redirect(url_for("login"))
+
     return render_template("register.html")
 
-@app.route("/login")
+@app.route("/login", methods = ["GET", "POST"])
 def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            print("Found user")
+        else:
+            print("User not found")
+
     return render_template("login.html")
 
 @app.route("/dashboard")
@@ -43,5 +72,7 @@ def dashboard():
 if __name__ == "__main__":  
     with app.app_context(): 
         db.create_all()
+        for user in User.query.all():
+            print(user.username)
         
     app.run(debug=True)
